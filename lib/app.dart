9057bdb747dbe4dd7a7baa9b2a3ddec901268664
a-tiny-developer <atinyflame@gmail.com>
2022-01-20 19:59:15 -0600

    Start of the project. Movies App

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'screens/screens.dart';
+
+class MoviesApp extends StatelessWidget {
+  const MoviesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Movies App',
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => const HomeScreen(),
+        'details': (context) => const DetailsScreen(),
+      },
+    );
+  }
+}
