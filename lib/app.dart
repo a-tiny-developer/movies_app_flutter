64884c909445e303149f08b3d4c175e46e81ad59
a-tiny-developer <atinyflame@gmail.com>
2022-01-21 20:16:35 -0600

    Provider and http package

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
 import 'router/router.dart';
 import 'theme/theme.dart';
+import 'providers/providers.dart';
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => MoviesProvider())
+      ],
+      child: const MoviesApp(),
+    );
+  }
+}
 
 class MoviesApp extends StatelessWidget {
   const MoviesApp({Key? key}) : super(key: key);
--- lib/main.dart
+++ lib/main.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 import 'app.dart';
 
-void main() => runApp(const MoviesApp());
+void main() => runApp(const AppState());
